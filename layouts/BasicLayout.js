1bf2eaf547de9fa402614bfa5935921df1c63b94
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-12-01 09:09:05 -0600

    render-children

---
+++ layouts/BasicLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function BasicLayout (props) {
+    const {children} = props
+ return(
+    <div>
+    <h1>Basic Layout</h1>
+    {children}
+
+</div>
+ ) 
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
 
+
+import BasicLayout from '../layouts/BasicLayout';
 
 export default function Home() {
   return (
-    <div className='home'>
+    <BasicLayout>
       <h1>Hello</h1>
-    </div>
+    </BasicLayout>
+  
   )
 }
