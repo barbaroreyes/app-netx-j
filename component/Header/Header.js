4cecf04efa8dc53a7c20c67994729a4159f5f86b
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-12-01 21:13:54 -0600

    added-top-bar

---
+++ component/Header/Header.js
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+  return (
+    <div className="header">
+        <div>TopBar</div>
+        <div>Menu</div>
+      
+    </div>
+  );
+}
+
--- layouts/BasicLayout.js
+++ layouts/BasicLayout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Container} from 'semantic-ui-react'
-
+import Header from '../component/Header/Header'
 export default function BasicLayout (props) {
     const {children} = props
  return(
     <Container fuild className= 'basic-layout'>
+     <Header/>   
     <Container className= 'content'>
     {children}
     </Container>
